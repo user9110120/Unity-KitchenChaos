d7ffc95fd67d9c4ab4b89899327e044d53e394c0
Neal <150328394+user9110120@users.noreply.github.com>
2026-03-20 14:53:23 +0800

    Add files via upload

---
+++ Assets/Scripts/ScriptableObject/AudioClipRefsSO.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class AudioClipRefsSO : ScriptableObject
+{
+
+    public AudioClip[] chop;
+    public AudioClip[] deliveryFail;
+    public AudioClip[] deliverySuccess;
+    public AudioClip[] footStep;
+    public AudioClip[] objectPickup;
+    public AudioClip[] objectDrop;
+    public AudioClip stoveSizzle;
+    public AudioClip[] trash;
+    public AudioClip[] warning;
+
+
+}
